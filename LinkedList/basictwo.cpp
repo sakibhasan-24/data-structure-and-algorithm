591203a92bd33dec65447d6f8e3e6767dede8108
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-04-14 19:49:19 +0600

    quick find md

--- LinkedList/basictwo.cpp
+++ LinkedList/basictwo.cpp
@@ -1,1 +1,34 @@
-//let's dive into doubly linked list
+// //let's dive into doubly linked list
+// class Node {
+//     public:
+//         int value;
+//         Node *prev;
+//         Node *next;
+
+//         Node (int data){
+//             value=data;
+//             prev=nullptr;
+//             next=nullptr;
+//         }
+// };
+
+#include<iostream>
+using namespace std;
+class  Node  {
+    int value;
+    Node *next;
+
+    Node (int data){
+        data=value;
+        next=NULL;
+    }
+ 
+};
+
+int main ()
+
+
+{
+
+    return 0;
+}
