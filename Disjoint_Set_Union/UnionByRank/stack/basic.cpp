f73d08d8634f656863e4c46d9d9f497e7299c060
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-04-20 02:46:24 +0600

    stack operation

--- Disjoint_Set_Union/UnionByRank/stack/basic.cpp
+++ Disjoint_Set_Union/UnionByRank/stack/basic.cpp
@@ -21,11 +21,59 @@ class Stack{
             top=-1;
             box=new int[size];
         }
+        ~Stack() {
+            delete[] box;
+        }
+        bool isEmpty(){
+            return top==-1;
+        }
+        bool isFull (){
+            return top==size-1;
+        }
+        void push(int value) {
+            if (isFull()) {
+                cout<< "Stack Overflow: Cannot add value " << value << "!\n";
+                return;
+            }
+            box[++top] = value;
+            cout << " Pushed " << value << " at position " << top << ".\n";
+        }
+    
+
+        void pop() {
+            if (isEmpty()) {
+                cout << " Stack Underflow: Stack is already empty!\n";
+                return;
+            }
+            cout << " Popped " << box[top--] << " from the stack.\n";
+        }
+        int peek()  {
+            if (isEmpty()) {
+                cerr << " Stack is Empty! Cannot peek.\n";
+                return -1; 
+            }
+            return box[top];
+        }
+        void display()  {
+            if (isEmpty()) {
+                cout << " Stack is empty.\n";
+                return;
+            }
+    
+            cout << "Stack contents (top to bottom): ";
+            for (int i = top; i >= 0; --i) {
+                cout << box[i] << " ";
+            }
+            cout << "\n";
+        }
+    
+        
 };
 
 int main ()
 
 
 {
-
+    Stack s(7);
+    s.pop();
 }
