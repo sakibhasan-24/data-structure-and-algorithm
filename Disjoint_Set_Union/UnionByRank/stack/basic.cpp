9a704822b788b0134d9381f5cc08c62a3cf34736
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-04-20 01:18:54 +0600

    design a stack using array

--- Disjoint_Set_Union/UnionByRank/stack/basic.cpp
+++ Disjoint_Set_Union/UnionByRank/stack/basic.cpp
@@ -1,3 +1,31 @@
 // Stack is a linear Data structure
 //follow LIFO
 //operations are =>push,pop,top,isEmpty
+
+
+
+#include<iostream>
+
+using namespace std;
+
+
+class Stack{
+    int *box;
+    int size;
+    int top;
+
+
+    public:
+        Stack(int s){
+           size=s;
+            top=-1;
+            box=new int[size];
+        }
+};
+
+int main ()
+
+
+{
+
+}
