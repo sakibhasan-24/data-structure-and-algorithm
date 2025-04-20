f52489f1233a4e039525e9a53893257313442cd3
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-04-20 12:09:55 +0600

    linked list of stack push and pop

--- Disjoint_Set_Union/UnionByRank/stack/basicTwo.cpp
+++ Disjoint_Set_Union/UnionByRank/stack/basicTwo.cpp
@@ -20,7 +20,27 @@ class Stack {
             top=NULL;
             size=0;
         }
+        //top=NULL,
+        //temp->next will point this NULL
+        //20->NULL
+        // top=temp
 
+        void push(int value){
+            Node *temp=new Node(value);
+            temp->next=top;
+            top=temp;
+            size++;
+        }
+        void pop(){
+            if(size==0 || top==NULL){
+                cout<<"Stack is Empty!"<<endl;
+                return;
+            }
+            Node *temp=top;
+            top=top->next;
+            size--;
+            delete temp;
+        }
     
 
 };
@@ -30,6 +50,8 @@ int main ()
 
 
 {
-
+    Stack s;
+    s.push(10);
+    s.pop();
 
 }
