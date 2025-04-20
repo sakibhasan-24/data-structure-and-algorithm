b8af4a06475aeaebc08d6da22caf3f2b91eec3b8
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-04-20 11:48:40 +0600

    design stack with LL

---
+++ Disjoint_Set_Union/UnionByRank/stack/basicTwo.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *next;
+        public:
+        Node(int val)
+        {
+            data=val;
+            next=NULL;
+        }
+};
+class Stack {
+    Node *top;
+    int size;
+    public:
+        Stack(){
+            top=NULL;
+            size=0;
+        }
+
+    
+
+};
+
+
+int main ()
+
+
+{
+
+
+}
