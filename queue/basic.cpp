9a9ae0d6a988ac341f9aa808c63083d53f73d182
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-04-21 19:54:40 +0600

    queue operation

--- queue/basic.cpp
+++ queue/basic.cpp
@@ -1,19 +1,56 @@
 #include<iostream>
 #include<vector>
+#include<string>
 using namespace std;
 
 //queue is a linear data structure
 //queue also can implement using array and linked list
 
+void message(string msg){
+    cout<<msg;
+}
 class Queue{
-    int line*;
+    int *arr;
     int front,rear;
     int size;
 
+    public:
     Queue(int n){
         size=n;
         front=-1,rear=-1;
-        line=new int [n];
+        arr=new int [n];
+    }
+
+    bool isEmpty(){
+        return rear==-1;
+    }
+    bool isFull(){
+        return rear==size-1;
+    }
+    //rear=-1,front=-1
+    void push(int value){
+        if(isFull()){
+            cout<<"Queue is Full!!!!!!!!!!!!"<<endl;
+            return;
+        }else if(isEmpty()){
+            front=rear=0;
+            arr[front]=value;
+        }else {
+            rear++;
+            arr[rear]=value;
+        }
+
+    }
+
+    void pop(){
+        if(isEmpty()){
+            message("Queue is Empty!");
+        }
+        else if(rear==front){
+            rear=front=-1;
+        }else {
+            front=front+1;
+        }
     }
 };
 
@@ -21,6 +58,9 @@ int main ()
 
 
 {
-
+    Queue q1(5);
+    q1.pop();
+    q1.push(100);
+    q1.pop();
     return 0;
 }
