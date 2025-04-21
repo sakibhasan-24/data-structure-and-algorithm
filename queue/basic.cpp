9098f395a3b7d41e8fd0e8396ffd8392c3832434
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-04-21 19:26:09 +0600

    design queue using array

---
+++ queue/basic.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//queue is a linear data structure
+//queue also can implement using array and linked list
+
+class Queue{
+    int line*;
+    int front,rear;
+    int size;
+
+    Queue(int n){
+        size=n;
+        front=-1,rear=-1;
+        line=new int [n];
+    }
+};
+
+int main ()
+
+
+{
+
+    return 0;
+}
