12b352fd6b6e999bafa7c2eed0ddd18c6bdd2e56
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-05-15 02:16:59 +0600

    binary tree create using level order

--- tree/binary-tree.cpp
+++ tree/binary-tree.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 using namespace std;
 
 // Binary tree is a tree data structure in which each node has at most two children.
 // Binary trees are used to represent hierarchical data, such as file systems, organizational structures, and more.
-
+ 
 class TreeNode {
     public:
         int val;
@@ -13,6 +14,7 @@ class TreeNode {
 
         TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
+ 
 
 
 TreeNode *createBinaryTree(){
@@ -28,10 +30,37 @@ TreeNode *createBinaryTree(){
     node->right = createBinaryTree();
     return node;
 }
+
+
+TreeNode *createBinaryTreeUsingQueue(){
+    int val;
+    cin>>val;
+    if(val==-1) return NULL;
+    TreeNode *root=new TreeNode(val);
+    queue <*TreeNode> child;
+    child.push(root);
+    int left,right;
+    while(!child.empty()){
+        // left.
+        TreeNode *temp=child.front();
+        child.pop();
+        cin>>left;
+        if(left!=-1){
+            temp->left=new TreeNode(left);
+            child.push(temp->left);
+        }
+        cin>>right;
+        if(right!=-1){
+            temp->right=new TreeNode(right);
+            child.push(temp->right);
+        }
+        
+    }
+    return root;
+
+}
 int main(){
     TreeNode *root;
     cout<<"Enter the value of the root node: "<<endl;
-    int val;
-    cin>>val;
     root=createBinaryTree();
 }
