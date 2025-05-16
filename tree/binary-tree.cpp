d970709c26dcbdb04ef4865e6b8ed56a3f88426d
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-05-16 12:34:31 +0600

    preOrder traversal

--- tree/binary-tree.cpp
+++ tree/binary-tree.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<stack>
 using namespace std;
 
 // Binary tree is a tree data structure in which each node has at most two children.
@@ -37,7 +38,7 @@ TreeNode *createBinaryTreeUsingQueue(){
     cin>>val;
     if(val==-1) return NULL;
     TreeNode *root=new TreeNode(val);
-    queue <*TreeNode> child;
+    queue <TreeNode*> child;
     child.push(root);
     int left,right;
     while(!child.empty()){
@@ -59,8 +60,28 @@ TreeNode *createBinaryTreeUsingQueue(){
     return root;
 
 }
+        void preOrder(TreeNode *root){
+            //output 1 2 4 5 3 6
+            //1 
+            //then store 1 and leave it
+            //print it's value
+            //then store 2
+            //first need to print root,then left then right
+            //i insert root then print it...
+            if (root==NULL) return;
+            stack<TreeNode*>s;
+            s.push(root);
+            while(!s.empty()){
+                TreeNode *node=s.top();
+                s.pop();
+                cout<<node->val <<" ";
+                if(node->right) s.push(node->right);
+                if(node->left) s.push(node->left);
+            }
+        }
 int main(){
     TreeNode *root;
     cout<<"Enter the value of the root node: "<<endl;
     root=createBinaryTree();
+    preOrder(root);
 }
