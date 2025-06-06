4aa4d4eedfb5b76d95607f3d677df70dd1559959
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-06-07 00:21:24 +0600

    bfs traversal

--- 2Dmatrix/bfs.cpp
+++ 2Dmatrix/bfs.cpp
@@ -22,14 +22,40 @@ void bfsInTwoDMatrix(vector<vector<int>>&matrix,int startRow,int startCol){
     // 4 5 6
     // 7 8 9
     // if i am at 0,1 that means 2 ,i can go to 0,2 (right) , 1,1 (down) , 0,0 (left) , -1,1 (up)
-    int row=matrix.szie();
-    if(row==0) return 0;
+    int row=matrix.size();
+    // if(row==0) return 0;
 
     int cols=matrix[0].size();
     int rowDirections[]={-1,1,0,0};
     int colDirections[]={0,0,-1,1};
     // mark all them visited?
-    vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+    vector<vector<bool>> visited(row, vector<bool>(cols, false));
+    queue<pair<int,int>> q;
+    visited[startRow][startCol]=true;
+    q.push({startRow,startCol});
+
+    while(!q.empty()){
+        auto[r,c]=q.front();
+        q.pop();
+        cout<<matrix[r][c]<<" ";
+        //please bro search your neighbour,,
+        for(int i=0;i<4;i++){
+            int newRow=r+rowDirections[i];
+            int newCol=c+colDirections[i];
+
+            //check their access
+            if(newRow>=0 && newRow<row && newCol>=0 && newCol<cols){
+                //they are in valid neighbour
+                if(!visited[newRow][newCol]){
+                    //they are not visited
+                    visited[newRow][newCol]=true;
+                    q.push({newRow,newCol});
+                }
+
+            }
+        }
+    }
+    
 }
 
 int main ()
@@ -60,4 +86,14 @@ int main ()
             cout<<"("<<newRow<<","<<newCol<<")"<<endl;
     }
     }
+
+
+    vector<vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    cout << "BFS Traversal starting from (0,0):\n";
+    bfsInTwoDMatrix(matrix,2,2);
 }
