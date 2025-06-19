75ab06d66030c86c600f8c43d29902977a0a4ac2
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-06-19 12:22:06 +0600

    maximumSubarraySum

--- technique/sliding-window/maximum-sum-of-subarray.cpp
+++ technique/sliding-window/maximum-sum-of-subarray.cpp
@@ -20,6 +20,7 @@ Output : 39 */
 #include<vector>
 #include<algorithm>
 #include<climits>
+#include<unordered_map>
 using namespace std;
 //O(n * k)
 int maximumOutput(vector<int>&nums,int k){
@@ -55,7 +56,76 @@ int slidingWindowSum(vector<int>&nums,int k){
     return maxSum;
     
 }
-                
+bool isValid(vector<int>&nums,int starting,int k){
+    int l=starting;
+    int r=starting+k-1;
+    unordered_set<int> visited;
+    while(l<=r){
+        if(visited.count(nums[l])){
+            return false;
+        }
+            visited.insert(nums[l]);
+            l++;
+        
+    }
+    return true;
+    
+}
+
+    //  [1,5,4,2,9,9,9], k = 3
+long long maximumSubarraySum(vector<int>& nums, int k) {
+    long long sum=0;
+    long long maxSum=0;
+    for(int i=0;i<k;i++){
+        sum+=nums[i];
+    }
+    if(isValid(nums,0,k)) {
+        maxSum=sum;
+    }
+    for(int i=k;i<nums.size();i++){
+        sum=sum+nums[i]-nums[i-k];
+        if(isValid(nums,i-k+1,k)){
+            maxSum=max(sum,maxSum);
+        }
+    }
+    return maxSum;
+        
+    }
+
+
+
+    //optimize 
+    //  vector<int> arr = {1, 4, 2, 10, 23, 3, 1, 0, 20};
+    1:1=>1
+    long long maximumSubarraySum(vector<int>& nums, int k){
+        unordered_map<int,int>mp;
+        long long sum=0;
+        long long maxSum=0;
+        int left=0;
+        for(int r=0;r<nums.size();r++){
+            int num=nums[r];
+            mp[num]++;
+            sum+=num;
+
+
+            if(r-left+1>k){
+                int leftNum=mp[left];
+                mp[leftNum]--;
+                sum-=leftNum;
+                if(mp[leftNum]==0){
+                    mp.erase(leftNum);
+                }
+                 left++;
+            }
+            if(r-left+1==k && mp.size()==k){
+                 maxSum = max(maxSum, sum);
+            }
+
+
+        }
+        return maxSum;
+    }
+
 int main ()
 
 
