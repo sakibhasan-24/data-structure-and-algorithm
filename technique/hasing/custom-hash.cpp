ae5f8cf5b76b3a088280f3a7dd8806ed94824aec
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-06-24 20:14:18 +0600

    structure of hashMap

---
+++ technique/hasing/custom-hash.cpp
@@ -0,0 +1,43 @@
+/* 
+A fixed-size array of buckets
+ Each bucket will store a list of key-value pairs (to handle collisions)
+ 
+ structure
+    key-value pair
+
+*/
+
+#include<iostream>
+#include<vector>    
+#include<list>
+using namespace std;
+
+
+
+struct KeyValue{
+    string key;
+    int value;
+
+    KeyValue(string k, int v){
+        key=k;
+        value=v;
+    }
+};
+class HashMap{
+    private:
+        int SIZE=10;
+        vector<list<KeyValue>>table;
+
+    public:
+        HashMap(){
+            table.resize(SIZE);
+        }
+}
+
+
+
+int main()
+
+{
+    
+}
