8c6d648bfc7ce122941c8aefa7b2733bace26e6b
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-06-27 15:48:29 +0600

    binary search koko eats banana

--- technique/binary-search/koko-eatc.cpp
+++ technique/binary-search/koko-eatc.cpp
@@ -20,8 +20,36 @@ int minEatingSpeed(vector<int>& piles, int h) {
             time = i;
             break;
         }
-
     }
+
     return time;
     
+}
+
+
+//koko can not eat eat 0 per hour,must eat at least one
+//
+
+
+int minEatingSpeed(vector<int>& piles, int h) {
+    int left=1;
+    int right = *max_element(piles.begin(), piles.end());
+    int res=right;
+
+
+    while(left<=right){
+        int mid=left+(right-left)/2;
+        int totalHours=0;
+        for(int i=0;i<piles.size();i++){
+            totalHours += ceil((double)piles[i]/mid);
+        }
+        if(totalHours<=h){
+            res = mid;
+            right = mid-1;
+        }
+        else
+            left = mid+1;
+    }
+    return res;
+
 }
