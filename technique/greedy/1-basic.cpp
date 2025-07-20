985efe59e1ee2f5694204aba65c67d8d635c99b3
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-07-20 21:39:58 +0600

    start greedy algorithm theory

---
+++ technique/greedy/1-basic.cpp
@@ -0,0 +1,9 @@
+/* 
+What is Greedy Algorithm?
+always makes the choice that seems to be the best at that moment.
+
+The	two	basic	properties	of	optimal	Greedy	algorithms	are:
+ 1)Greedy	choice	property
+ 2)Optimal	substructure
+
+*/
