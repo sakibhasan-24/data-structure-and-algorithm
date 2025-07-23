6878abb5c116613e33e4dd2c449ca03f1c3d4fab
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-07-23 21:40:54 +0600

    dry run

--- technique/greedy/2-selection-activity.cpp
+++ technique/greedy/2-selection-activity.cpp
@@ -12,7 +12,31 @@ using namespace std;
 // Time Complexity: O(nlogn)
 // Space Complexity: O(1)
 
-struct Activity {
-    int start;
-    int end;
-}
+
+/* 
+
+Activities:  (start, end)
+(1, 2)
+(3, 4)
+(0, 6)
+(5, 7)
+(8, 9)
+(5, 9)
+
+
+sorted by Sorted by end: (1, 2), (3, 4), (0, 6), (5, 7), (5, 9), (8, 9)
+
+Select (1, 2) → finishes at 2
+
+Next is (3, 4) → starts at 3 ≥ 2 →  Select
+
+Next is (0, 6) → starts at 0 < 4 →  Skip
+
+Next is (5, 7) → starts at 5 ≥ 4 →  Select
+
+Next is (5, 9) → starts at 5 < 7 →  Skip
+
+Next is (8, 9) → starts at 8 ≥ 7 →  Select
+
+ Selected: (1,2), (3,4), (5,7), (8,9) → 4 activities
+*/
