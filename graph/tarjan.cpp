725b76ac577bf92e640b2be2e6155ca543af4ec9
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-01-03 23:32:15 +0600

    a short note

--- graph/tarjan.cpp
+++ graph/tarjan.cpp
@@ -48,3 +48,7 @@ vector<vector<int>> tarjansSCC(int V, vector<int> adj[]) {
     }
     return result;
 }
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000
+   OR population >= 25000000;
